ab2752ce0df3d390a18b84bf49a872305f4bfa11
tansinjahan <tansin.jahan@gmail.com>
2017-09-25 16:50:48 -0400

    Initial project

---
+++ src/tansinjahan/tdd/assignment/University.java
@@ -0,0 +1,7 @@
+package tansinjahan.tdd.assignment;
+
+public class University {
+	public String getName() {
+		return "Carleton";
+	}
+}
---
+++ src/tansinjahan/tdd/assignment/test/UniversityTest.java
@@ -0,0 +1,20 @@
+package tansinjahan.tdd.assignment.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tansinjahan.tdd.assignment.University;
+
+public class UniversityTest {
+
+	@Test
+	public void testName() {
+		University versity = new University();
+		assertEquals("Carleton", versity.getName());
+	}
+	public void getMethod()
+	{
+		
+	}
+}
