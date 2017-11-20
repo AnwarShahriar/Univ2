555cd611c57a79312b73c9c6cda00840e3e1b1d9
Md. Shahriar Anwar <anwarshahriar@localhost.localdomain>
2017-11-21 03:21:02 +0600

    Added fundamental server

--- src/server/network/StartServer.java
+++ src/server/network/StartServer.java
@@ -1,10 +1,37 @@
 package server.network;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import gherkin.ast.Location;
+import io.javalin.ApiBuilder.EndpointGroup;
+import io.javalin.Context;
+import io.javalin.Handler;
+import io.javalin.Javalin;
 import utilities.Config;
 
 public class StartServer {
 	public static void main(String[] argv) {
-		System.out.println("Starting server ...");
-		new LibServer(Config.DEFAULT_PORT);
+		final Javalin app = Javalin.create()
+	            .port(Config.DEFAULT_PORT)
+	            .enableStaticFiles("/public")
+	            .start();
+		
+		app.routes(new EndpointGroup() {
+			
+			@Override
+			public void addEndpoints() {
+				app.get("/", new Handler() {
+					
+					@Override
+					public void handle(Context ctx) throws Exception {
+						Map<String, String> map = new HashMap<String, String>();
+						ctx.renderVelocity("/velocity/home.vm");
+					}
+				});
+			}
+		});
 	}
 }
